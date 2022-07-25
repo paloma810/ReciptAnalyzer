26b465d950ad32400eeadf40f4ce563c6dcecc27
paloma810 <mcyk49129@gmail.com>
2022-07-25 23:18:01 +0900

    Initial Commit

---
+++ ReciptAnalyzer/ReciptAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciptAnalyzerApp.swift
+//  ReciptAnalyzer
+//
+//  Created by Kenta Machida on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ReciptAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
